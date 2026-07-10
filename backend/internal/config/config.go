4d2bc504fc29858552fdd728ef97adff802a44ce
Tylerw7 <t4wettach@gmail.com>
2026-07-10 11:17:09 -0700

    added files to backend

---
+++ backend/internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port       string
+	DBHost     string
+	DBPort     string
+	DBName     string
+	DBUser     string
+	DBPassword string
+	DBSSLMode  string
+}
+
+func Load() *Config {
+	godotenv.Load()
+
+	return &Config{
+		Port:       os.Getenv("PORT"),
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBSSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+}
