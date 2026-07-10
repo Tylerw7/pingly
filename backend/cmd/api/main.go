276a8aea7eb0ca4e87790fdcbf8acccbab984fe0
Tylerw7 <t4wettach@gmail.com>
2026-07-10 11:38:40 -0700

    created docker files

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Gin API is running",
+		})
+	})
+
+	r.Run(":8080")
 }
