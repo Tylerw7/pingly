d65fe00908c4c2529e175181e9e08a6ac1a81e7f
Tylerw7 <t4wettach@gmail.com>
2026-07-15 21:01:10 -0700

    added hub

--- backend/internal/hub/hub.go
+++ backend/internal/hub/hub.go
@@ -12,3 +12,25 @@ func New() *Hub {
 		topics: make(map[string]map[chan []byte]struct{}),
 	}
 }
+
+func (h *Hub) Subscribe(topic string) (ch chan []byte, unsubscribe func()) {
+
+	ch = make(chan []byte, 16)
+
+	h.mu.Lock()
+	if h.topics[topic] == nil {
+		h.topics[topic] = make(map[chan []byte]struct{})
+	}
+	h.topics[topic][ch] = struct{}{}
+	h.mu.Unlock()
+
+	unsubscribe = func() {
+		h.mu.Lock()
+		delete(h.topics[topic], ch)
+		if len(h.topics[topic]) == 0 {
+			delete(h.topics, topic)
+		}
+		h.mu.Unlock()
+	}
+	return ch, unsubscribe
+}
