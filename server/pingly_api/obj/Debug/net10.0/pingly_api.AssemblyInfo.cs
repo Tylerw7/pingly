6ece90e3a4b62443fca2fe93e34ee1436ff3c256
Tylerw7 <t4wettach@gmail.com>
2026-07-25 16:16:15 -0700

    added hub class

--- server/pingly_api/obj/Debug/net10.0/pingly_api.AssemblyInfo.cs
+++ server/pingly_api/obj/Debug/net10.0/pingly_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pingly_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c375b51f4960594c776967dd11e40eee4a0912f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5df14b6a04004ec99208e0038e72c1802df1d9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("pingly_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pingly_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
